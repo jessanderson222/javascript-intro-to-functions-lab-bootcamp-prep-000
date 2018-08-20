d308a0108c46e097546d680baf18dcdba28203e6
JESSIE ANDERSON <jessanderson222@gmail.com>
2018-08-20 00:16:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log(string.toUpperCase)
+  console.log(string.toUpperCase) === 'HELLO'
 }
 function sayHiToGrandma(string) {
   if (string.toUpperCase()===string)
