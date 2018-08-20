ad2f030eeb255ac6200717ec8343d5a1d44b4d23
JESSIE ANDERSON <jessanderson222@gmail.com>
2018-08-20 00:13:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function logShout(string === 'HELLO') {
+function logShout(string) string.toUpperCase === 'HELLO' {
   console.log("YES INDEED!")
 }
 function sayHiToGrandma(string) {
